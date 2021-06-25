b69eeaf80a0eaa2f2cb02f0ac2bf726ca8662618
alialaghbandrad <72175406+alialaghbandrad@users.noreply.github.com>
2021-06-25 17:52:17 -0400

    Add files via upload

---
+++ Day05Myex6_1AssignGrades/src/day05myex6_1assigngrades/Day05Myex6_1AssignGrades.java
@@ -0,0 +1,16 @@
+package day05myex6_1assigngrades;
+
+import java.util.Scanner;
+
+public class Day05Myex6_1AssignGrades {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of students: ");
+        int count = input.nextInt();
+        for (int i = 0; i < count; i++) {
+            
+        }
+    }
+    
+}
