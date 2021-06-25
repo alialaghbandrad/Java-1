87fd04becea0cfc3ed7da20bc975674cc90c136d
alialaghbandrad <72175406+alialaghbandrad@users.noreply.github.com>
2021-06-25 17:45:01 -0400

    Add files via upload

---
+++ Day01Hello/src/day01hello/Day01Hello.java
@@ -0,0 +1,11 @@
+package day01hello;
+
+
+public class Day01Hello {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Look, I am programming Java");
+    }  
+}
