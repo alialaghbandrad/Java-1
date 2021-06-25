d57cd705c6d2ea56cd175ff5adb732e1c21df4e6
alialaghbandrad <72175406+alialaghbandrad@users.noreply.github.com>
2021-06-25 17:51:36 -0400

    Add files via upload

---
+++ Day04Myhw4_18FourPatterns/src/day04myhw4/pkg18fourpatterns/Day04Myhw418FourPatterns.java
@@ -0,0 +1,13 @@
+package day04myhw4.pkg18fourpatterns;
+
+
+public class Day04Myhw418FourPatterns {
+
+    public static void main(String[] args) {
+        System.out.println("Pattern A   Pattern B   Pattern C   Pattern D");
+        for (int i = 0; i < 6; i++) {
+            System.out.printf("%d\n", i);
+        }
+    }
+    
+}
