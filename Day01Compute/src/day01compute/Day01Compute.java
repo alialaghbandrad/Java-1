76d5c4d1a85891fe89aa281575dba69440d4b123
alialaghbandrad <72175406+alialaghbandrad@users.noreply.github.com>
2021-06-25 17:45:51 -0400

    Add files via upload

---
+++ Day01Compute/src/day01compute/Day01Compute.java
@@ -0,0 +1,15 @@
+package day01compute;
+
+ class Day01Compute {
+
+    public static void main(String[] args) {
+        double radius;
+        double area;
+        
+        radius = 20;
+        area = radius * radius * 3.14159;
+                
+        System.out.println("The area for the circle of radius " + radius + " is " + area);
+    }
+    
+}
